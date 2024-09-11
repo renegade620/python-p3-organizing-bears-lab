7175ce895faa7fa26a65264488aabc388d8e4043
Frankline Were <franklinegift@gmail.com>
2024-09-11 09:09:25 +0300

    insert into table

---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears(name, age, sex, color, temperament, alive) VALUES 
+("Mr. Chocolate", 5, "M", "Black", "Mild", 1),
+("Rowdy", 5, "M", "Brown", "Mild", 1),
+("Tabitha", 5, "F", "Black", "Mild", 1),
+("Sergeant Brown", 5, "M", "Black", "Mild", 1),
+("Melissa", 5, "F", "Red", "Mild", 0),
+("Grinch", 5, "M", "Black", "Mild", 1),
+("Wendy", 5, "F", "Black", "Mild", 1),
+(NULL, NULL, "M", "Black", "Mild", 1);
+
